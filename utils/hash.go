196e6de6702de19cc583678b684f0e8bbfab9f3e
He Sheng <sheng_he@kingdee.com>
2018-01-11 20:01:57 +0800

    Update hash.go

--- utils/hash.go
+++ utils/hash.go
@@ -1,28 +1,10 @@
 package utils
 
 import (
-	"bytes"
 	"crypto/sha256"
-	"io"
-	"strconv"
+	"fmt"
 )
 
 func CalculateHash(any string) string {
-	hs := sha256.New()
-	io.WriteString(hs, any)
-	return ByteToHex(hs.Sum(nil))
-}
-
-func ByteToHex(data []byte) string {
-	buffer := new(bytes.Buffer)
-	for _, b := range data {
-
-		s := strconv.FormatInt(int64(b&0xff), 16)
-		if len(s) == 1 {
-			buffer.WriteString("0")
-		}
-		buffer.WriteString(s)
-	}
-
-	return buffer.String()
+	return fmt.Sprintf("%x", sha256.Sum256([]byte(any)))
 }
