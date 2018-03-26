bc33d65f8f5bf0f4fba93b5464528e086a8c1406
He Sheng <sheng_he@kingdee.com>
2018-03-26 13:42:05 +0800

    data可选

--- controllers/miner.go
+++ controllers/miner.go
@@ -13,7 +13,7 @@ type MinerController struct {
 
 // @Title Get
 // @Description mine new block
-// @Param	data	path	string	true	"the data you want to write into blockchain"
+// @Param	data	path	string	fasle	"the data you want to write into blockchain"
 // @Success 200 {Block} block.Block
 // @Failure 403 invalid
 // @router /:data [get]
